65d57527da62bf1190606a30a1fa19771fa4b157
Drew Olsen <olsenj.andrew@gmail.com>
2021-04-01 09:37:53 -0500

    main work file generated. Started full md template.

--- index.js
+++ index.js
@@ -20,31 +20,99 @@ WHEN I enter my email address
 THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
 WHEN I click on the links in the Table of Contents
 THEN I am taken to the corresponding section of the README
+///////Questions to Collect
+1.Project Title
+2.Description
+3.Installation section should be a default, with just some minor adjustments
+4.Github username. have default githuburl, just get username
+5.Email
+6.List of options for license.
+
+Initial Thoughts:
+Not sure if I should wrap the entire thing in template literals, try and see.
+Lets add stuff to auto find gif or png screenshot in assets folder
+
+
+//Here are the licenses
+MIT
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+APACHE
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+MOZILLA PUBLIC LICENSE 2.0
+[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+THE UNLICENSE
+[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+GNU GPL v2
+[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)
+
+
+
+Need to figure out how to create a table in .md
+*/
+
+/* //////sections needed
+Description
+Table of Contents
+Installation
+Usage
+License
+Contributing
+Tests
+Questions
+
+
 
 */
 
-inquirer
-	.prompt([
-		{
-			type: 'input',
-			message: 'What is your name?',
-			name: 'name',
-		},
-		{
-			type: 'password',
-			message: 'What languages do you know?',
-			name: 'knownLanguages',
-		},
-		{
-			type: 'password',
-			message: 'What is your preferred method of communication?',
-			name: 'preferredComms',
-		},
-	])
-	.then(answers => {
-		fs.appendFile(userInfo.txt, 'data to append', (err) => {
+//input - Possible values: input, number, confirm, list, rawlist, expand, checkbox, password, editor
+const questions = [
+	{
+		type: 'input',
+		message: 'Project Title',
+		name: 'projectTitle',
+	},
+	{
+		type: 'editor',
+		message: 'Give a project description.',
+		name: 'description',
+	},
+	{
+		type: 'editor',
+		message: 'Installation instructions',
+		name: 'install',
+	},
+	{
+		type: 'list',
+		message: 'License Type',
+		name: 'license',
+	},
+	{
+		type: 'editor',
+		message: 'Installation instructions',
+		name: 'installation',
+	},
+	{
+		type: 'editor',
+		message: 'Usage Information',
+		name: 'usageInfo',
+	}
+	
+]
 
-		})
+
+
+
+// fs.appendFile(userInfo.txt, 'data to append', (err) => {
+
+// })
+
+inquirer
+	.prompt(questions)
+
+
+	.then(answers => {
+		console.log(answers)
+		
 	})
 	.catch(error => {
 		if (error.isTtyError) {
@@ -56,14 +124,14 @@ inquirer
 
 
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// // TODO: Create an array of questions for user input
+// const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
 
 
